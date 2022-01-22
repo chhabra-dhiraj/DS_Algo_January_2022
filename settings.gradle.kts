815839e150ee31d928ecf644d6fd07f8f2bbba67
Dhiraj Chhabra <dhirajchhabra@Dhirajs-Mac.local>
2022-01-22 19:59:52 +0100

    Starting of DS and Algo Journey 2022

---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "DS_Algo_Liebherr"
+
---
+++ src/main/java/Recursion_January_22_2022/Recursion_Main.java
@@ -0,0 +1,8 @@
+package Recursion_January_22_2022;
+
+public class Recursion_Main {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World");
+    }
+}
