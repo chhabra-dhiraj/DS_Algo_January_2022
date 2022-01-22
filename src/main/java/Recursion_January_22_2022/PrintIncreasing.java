d941fc9d279d8f2752340c9c460a00d0bba59700
Dhiraj Chhabra <dhirajchhabra@Dhirajs-Mac.local>
2022-01-22 23:26:32 +0100

    added code for printIncreasing

---
+++ src/main/java/Recursion_January_22_2022/PrintIncreasing.java
@@ -0,0 +1,20 @@
+package Recursion_January_22_2022;
+
+import java.util.Scanner;
+
+public class PrintIncreasing {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        printIncreasing(n);
+    }
+
+    private static void printIncreasing(int n) {
+        if(n == 0) {
+            return;
+        }
+        printIncreasing(n - 1);
+        System.out.println(n);
+    }
+}
