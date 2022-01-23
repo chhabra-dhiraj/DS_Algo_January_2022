685f77ba56494ae6c7a14d6f5e391106f8972629
Dhiraj Chhabra <dhirajchhabra@Dhirajs-Mac.local>
2022-01-23 15:24:39 +0100

    Starting code for January 23, 2022

---
+++ src/main/java/TimeAndSpaceComplexity_January_23_2022/TASC_Main.java
@@ -0,0 +1,12 @@
+package TimeAndSpaceComplexity_January_23_2022;
+
+import java.util.Scanner;
+
+public class TASC_Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+    }
+
+}
